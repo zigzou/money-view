d71d53a8dfcc0e3341c9d625f9e35c9b576f458a
naoto <naoto0101@gmail.com>
2014-04-08 14:40:41 +0900

    commit init

---
+++ MoneyView/lib/src/main/java/jp/co/misyobun/moneyview/lib/MoneyView.java
@@ -0,0 +1,14 @@
+package jp.co.misyobun.moneyview.lib;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by takahashinaoto on 2014/04/08.
+ */
+public class MoneyView extends View {
+    public MoneyView(Context context) {
+        super(context);
+    }
+}
+
